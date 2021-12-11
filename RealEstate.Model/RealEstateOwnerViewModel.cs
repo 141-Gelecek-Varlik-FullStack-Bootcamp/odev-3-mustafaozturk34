fd6d5dd7ab2dd9a194f9f5522ebcddf944abe8a3
Muhammet Mustafa ÖZTÜRK <77894227+mustafaozturk34@users.noreply.github.com>
2021-12-11 19:33:56 +0300

    Model layer created

---
+++ RealEstate.Model/RealEstateOwnerViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealEstate.Model
+{
+    public class RealEstateOwnerViewModel
+    {
+        public string Name { get; set; }
+        public decimal Fortune { get; set; }
+        public decimal? RemainingSalary { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ RealEstate.Model/RealEstateViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealEstate.Model
+{
+    public class RealEstateViewModel
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Type { get; set; }
+        public decimal SquareMeters { get; set; }
+        public int Iuser { get; set; }
+    }
+}
